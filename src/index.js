43899373643bef86749fab1088bc7819ac670791
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-01-28 08:18:03 -0600

    Add images in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 // Leaderboard APP
 import './styles/main.css'
+import leaderboard from './images/leaderboard1.png'
+import Github from './images/github.svg'
+
+const loadLeaderHead = () => {
+  const leaderHead = document.getElementById('leader');
+  leaderHead.src = leaderboard;
+  leaderHead.alt = 'leaderboard';
+  leaderHead.setAttribute('class', 'header-icon');
+};
+
+const loadGithubIcon = () => {
+  const github = document.getElementById('github');
+  github.src = Github;
+  github.alt = 'github';
+  github.setAttribute('class', 'github-icon');
+};
+
+
+window.onload = () => {
+  loadLeaderHead();
+  loadGithubIcon();
+};
