ced336e9a0af5529049e833493d9330efeddc310
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-01-31 09:04:10 -0600

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Leaderboard APP
-import './styles/main.css'
-import Github from './images/github.svg'
+import './styles/main.css';
+import Github from './images/github.svg';
 
 const loadGithubIcon = () => {
   const github = document.getElementById('github');
@@ -8,7 +8,6 @@ const loadGithubIcon = () => {
   github.alt = 'github';
   github.setAttribute('class', 'github-icon');
 };
-
 
 window.onload = () => {
   loadGithubIcon();
